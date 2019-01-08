52c34150d5254d403666037aea1f8afbf10e137c
Eric Webb <eric.webb@minted.com>
2019-01-07 17:43:38 -0800

    Working material-ui/system Box example

--- README.md
+++ README.md
@@ -2,3 +2,7 @@ Material-UI System POC
 ======================
 
 A repo to help me try to grok https://material-ui.com/system
+
+## References
+
+* [Material-UI Github](https://github.com/mui-org/material-ui)
--- src/ExampleMuiBox.js
+++ src/ExampleMuiBox.js
@@ -1,7 +1,5 @@
 import React, { Component } from 'react';
 
-import ExampleMuiBox from './ExampleMuiBox';
-
 import { palette, spacing, typography } from '@material-ui/system';
 // import styled from '@emotion/styled';
 
@@ -12,10 +10,17 @@ import { unstable_Box as Box } from '@material-ui/core/Box';
 
 class App extends Component {
   render() {
-    return ([
-      <h1>Material-UI Box Example</h1>,
-      <ExampleMuiBox />,
-    ]);
+    return (
+      <Box
+        color="primary.main"
+        bgcolor="background.paper"
+        fontFamily="h6.fontFamily"
+        fontSize={{ xs: 'h6.fontSize', sm: 'h4.fontSize', md: 'h3.fontSize' } }
+        p={{ xs: 2, sm: 3, md: 4} }
+      >
+        @material-ui/system
+      </Box>
+    );
   }
 }
 
